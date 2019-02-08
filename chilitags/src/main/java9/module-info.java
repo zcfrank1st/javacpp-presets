df14cdb3f9ae3b10148da0d8ccf4e6e741dbdf5f
Samuel Audet <samuel.audet@gmail.com>
2019-02-08 15:37:20 +0900

    Modularize presets for ARToolKitPlus, Chilitags, and flandmark

---
+++ chilitags/src/main/java9/module-info.java
@@ -0,0 +1,5 @@
+module org.bytedeco.chilitags {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.chilitags.global;
+  exports org.bytedeco.chilitags;
+}
---
+++ flandmark/src/main/java9/module-info.java
@@ -0,0 +1,5 @@
+module org.bytedeco.flandmark {
+  requires transitive org.bytedeco.javacpp;
+  exports org.bytedeco.flandmark.global;
+  exports org.bytedeco.flandmark;
+}
